005ee0fb39cf5a912a95a717955f04ef807c2c1c
coderbot <coderbot16@gmail.com>
2020-01-25 20:01:45 -0800

    Handle wildcards in generic event signatures slightly more gracefully

--- src/main/java/com/patchworkmc/event/EventHandlerScanner.java
+++ src/main/java/com/patchworkmc/event/EventHandlerScanner.java
@@ -7,6 +7,8 @@ import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
+import com.patchworkmc.Patchwork;
+
 public class EventHandlerScanner extends ClassVisitor {
 	private Consumer<EventBusSubscriber> subscriberConsumer;
 	private Consumer<SubscribeEvent> subscribeEventConsumer;
@@ -143,12 +145,16 @@ public class EventHandlerScanner extends ClassVisitor {
 				// type arguments Or, if the descriptor does not start and end with L and ; that
 				// means that there are primitive type arguments
 
-				if ((genericClass.indexOf(';') != genericClass.lastIndexOf(';')) || !genericClass.startsWith("L") || !genericClass.endsWith(";")) {
-					throw new IllegalArgumentException("Generic events may only have one type parameter, but " + name + " uses an event with multiple (Signature: " + this.signature + ")");
-				}
+				if (genericClass.contains("*")) {
+					Patchwork.LOGGER.error("FIXME: Not sure how to handle wildcards! We need to implement proper signature parsing: %s", genericClass);
 
-				// Remove L and ;
-				genericClass = genericClass.substring(1, genericClass.length() - 1);
+					genericClass = null;
+				} else if ((genericClass.indexOf(';') != genericClass.lastIndexOf(';')) || !genericClass.startsWith("L") || !genericClass.endsWith(";")) {
+					throw new IllegalArgumentException("Generic events may only have one type parameter, but " + name + " uses an event with multiple (Signature: " + this.signature + ")");
+				} else {
+					// Remove L and ;
+					genericClass = genericClass.substring(1, genericClass.length() - 1);
+				}
 			}
 
 			return new SubscribeEventHandler(this.access, this.name, eventClass, genericClass, hasReturnValue, subscribeEventConsumer);
