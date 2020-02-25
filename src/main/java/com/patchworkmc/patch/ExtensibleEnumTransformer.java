23b06180209dd4b6b385dff4820cf5f206bcb343
Nuclearfarts <Nuclearfarts@users.noreply.github.com>
2020-02-25 09:47:03 -0500

    changes from review

--- src/main/java/com/patchworkmc/patch/ExtensibleEnumTransformer.java
+++ src/main/java/com/patchworkmc/patch/ExtensibleEnumTransformer.java
@@ -10,17 +10,17 @@ import org.objectweb.asm.Opcodes;
 public class ExtensibleEnumTransformer extends ClassVisitor {
 	private static final String PATCHWORK_ENUM_HACKS = "com/patchworkmc/api/enumhacks/EnumHacks";
 
-	//enum class -> method name on EnumHacks
+	// enum class -> method name on EnumHacks
 	private static final Map<String, String> redirects = new HashMap<>();
 
 	static {
-		redirects.put("net/minecraft/class_1814", "createRarity"); //Rarity
-		redirects.put("net/minecraft/class_1311", "createEntityCategory"); //EntityCategory
-		redirects.put("net/minecraft/class_3785$class_3786", "createStructurePoolProjection"); //StructurePool.Projection
-		redirects.put("net/minecraft/class_3124$class_3125", "createOreFeatureConfigTarget"); //OreFeatureConfig.Target
-		redirects.put("net/minecraft/class_2582", "createBannerPattern"); //BannerPattern
-		redirects.put("net/minecraft/class_1317$class_1319", "createSpawnRestrictionLocation"); //SpawnRestriction.Location
-		redirects.put("net/minecraft/class_1886", "createEnchantmentTarget"); //EnchantmentTarget
+		redirects.put("net/minecraft/class_1814", "createRarity"); // Rarity
+		redirects.put("net/minecraft/class_1311", "createEntityCategory"); // EntityCategory
+		redirects.put("net/minecraft/class_3785$class_3786", "createStructurePoolProjection"); // StructurePool.Projection
+		redirects.put("net/minecraft/class_3124$class_3125", "createOreFeatureConfigTarget"); // OreFeatureConfig.Target
+		redirects.put("net/minecraft/class_2582", "createBannerPattern"); // BannerPattern
+		redirects.put("net/minecraft/class_1317$class_1319", "createSpawnRestrictionLocation"); // SpawnRestriction.Location
+		redirects.put("net/minecraft/class_1886", "createEnchantmentTarget"); // EnchantmentTarget
 	}
 
 	public ExtensibleEnumTransformer(ClassVisitor classVisitor) {
@@ -39,8 +39,10 @@ public class ExtensibleEnumTransformer extends ClassVisitor {
 
 		@Override
 		public void visitMethodInsn(int opcode, String owner, String name, String descriptor, boolean isInterface) {
-			if (opcode == Opcodes.INVOKESTATIC && name.equals("create") && redirects.containsKey(owner)) {
-				super.visitMethodInsn(Opcodes.INVOKESTATIC, PATCHWORK_ENUM_HACKS, redirects.get(owner), descriptor, false);
+			String methodName;
+
+			if (opcode == Opcodes.INVOKESTATIC && name.equals("create") && (methodName = redirects.get(owner)) != null) {
+				super.visitMethodInsn(Opcodes.INVOKESTATIC, PATCHWORK_ENUM_HACKS, methodName, descriptor, false);
 				return;
 			}
 
