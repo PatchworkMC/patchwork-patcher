c8d0158b5d519d1527fb716c30b1b1258a365d48
coderbot <coderbot16@gmail.com>
2020-03-24 15:38:19 -0700

    Begin writing invokedynamic instructions for static event handlers

--- src/main/java/com/patchworkmc/event/EventHandlerScanner.java
+++ src/main/java/com/patchworkmc/event/EventHandlerScanner.java
@@ -1,24 +1,42 @@
 package com.patchworkmc.event;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 import org.objectweb.asm.AnnotationVisitor;
 import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.Handle;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
 
 import com.patchworkmc.Patchwork;
 
 public class EventHandlerScanner extends ClassVisitor {
+	private static final String REGISTER_STATIC = "patchwork_registerStaticEventHandlers";
+	private static final String REGISTER_INSTANCE = "patchwork_registerInstanceEventHandlers";
+	private static final String METAFACTORY_OWNER = "java/lang/invoke/LambdaMetafactory";
+	private static final String METAFACTORY_DESCRIPTOR = "(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;Ljava/lang/invoke/MethodType;Ljava/lang/invoke/MethodHandle;Ljava/lang/invoke/MethodType;)Ljava/lang/invoke/CallSite;";
+	private static final String EVENT_BUS = "net/minecraftforge/eventbus/api/IEventBus";
+
 	private Consumer<EventBusSubscriber> subscriberConsumer;
 	private Consumer<SubscribeEvent> subscribeEventConsumer;
+	private List<SubscribeEvent> staticSubscribers;
 	private String className;
 
 	public EventHandlerScanner(ClassVisitor parent, Consumer<EventBusSubscriber> subscriberConsumer, Consumer<SubscribeEvent> subscribeEventConsumer) {
 		super(Opcodes.ASM7, parent);
 
 		this.subscriberConsumer = subscriberConsumer;
-		this.subscribeEventConsumer = subscribeEventConsumer;
+		this.subscribeEventConsumer = subscribeEvent -> {
+			if ((subscribeEvent.getAccess() & Opcodes.ACC_STATIC) != 0) {
+				staticSubscribers.add(subscribeEvent);
+			} else {
+				subscribeEventConsumer.accept(subscribeEvent);
+			}
+		};
+		this.staticSubscribers = new ArrayList<>();
 	}
 
 	@Override
@@ -39,9 +57,45 @@ public class EventHandlerScanner extends ClassVisitor {
 
 	@Override
 	public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
+		if (name.equals(REGISTER_STATIC) || name.equals(REGISTER_INSTANCE)) {
+			throw new IllegalArgumentException("Class already contained a method named " + name + ", this name is reserved by Patchwork!");
+		}
+
 		MethodVisitor parent = super.visitMethod(access, name, descriptor, signature, exceptions);
 
 		return new MethodScanner(parent, access, name, descriptor, signature);
+	}
+
+	@Override
+	public void visitEnd() {
+		// Write patchwork_registerStaticEventHandlers
+
+		if (staticSubscribers.isEmpty()) {
+			super.visitEnd();
+
+			return;
+		}
+
+		Handle metafactory = new Handle(Opcodes.H_INVOKESTATIC, METAFACTORY_OWNER, "metafactory", METAFACTORY_DESCRIPTOR, false);
+		MethodVisitor staticRegistrar = super.visitMethod(Opcodes.ACC_PRIVATE | Opcodes.ACC_STATIC, REGISTER_STATIC, "(L" + EVENT_BUS + ";)V", null, null);
+
+		if (staticRegistrar != null) {
+			staticRegistrar.visitCode();
+
+			for (SubscribeEvent subscriber: staticSubscribers) {
+				Handle handler = new Handle(Opcodes.H_INVOKEVIRTUAL, className, subscriber.getMethod(), subscriber.getMethodDescriptor(), false);
+
+				staticRegistrar.visitVarInsn(Opcodes.ALOAD, 0);
+				staticRegistrar.visitInvokeDynamicInsn("accept", "()Ljava/util/function/Consumer;", metafactory, Type.getMethodType("(Ljava/lang/Object;)V"), handler, Type.getMethodType(subscriber.getMethodDescriptor()));
+				staticRegistrar.visitMethodInsn(Opcodes.INVOKEINTERFACE, EVENT_BUS, "addListener", "(Ljava/util/function/Consumer;)V", true);
+			}
+
+			staticRegistrar.visitInsn(Opcodes.RETURN);
+			staticRegistrar.visitMaxs(2, 1);
+			staticRegistrar.visitEnd();
+		}
+
+		super.visitEnd();
 	}
 
 	public class MethodScanner extends MethodVisitor {
