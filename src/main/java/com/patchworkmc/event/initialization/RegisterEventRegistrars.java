adcbab835d53bb2c7d7c6b63f54673b7368c361a
TheGlitch76 <glitchieproductionsofficial@gmail.com>
2020-06-10 20:39:00 -0500

    Fix another oopsie

--- src/main/java/com/patchworkmc/event/initialization/RegisterEventRegistrars.java
+++ src/main/java/com/patchworkmc/event/initialization/RegisterEventRegistrars.java
@@ -34,7 +34,7 @@ public class RegisterEventRegistrars implements Consumer<MethodVisitor> {
 			if (subscriber.hasInstanceSubscriber()) {
 				method.visitVarInsn(Opcodes.ALOAD, 1); // Push the instance to the stack (1)
 				method.visitLdcInsn(Type.getObjectType(className)); // Push the class to the stack (2)
-				LambdaUtil.visitBiConsumerStaticMethodReference(method, className, EventConstants.REGISTER_STATIC, EventConstants.REGISTER_STATIC_DESC, subscriber.isInterface());
+				LambdaUtil.visitBiConsumerStaticMethodReference(method, className, EventConstants.REGISTER_INSTANCE, EventConstants.getRegisterInstanceDesc(className), subscriber.isInterface());
 				// Pop the instance for calling, and then pop the class lambda as parameters
 				method.visitMethodInsn(Opcodes.INVOKEINTERFACE, EVENT_REGISTRAR_REGISTRY, "registerInstance", "(Ljava/lang/Class;Ljava/util/function/BiConsumer;)V", true);
 			}
