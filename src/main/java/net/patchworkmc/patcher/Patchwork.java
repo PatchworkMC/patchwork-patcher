a5d5908eaa26cce531a6e26f88bb6579bb05a72c
TheGlitch76 <glitchieproductionsofficial@gmail.com>
2020-09-26 12:32:25 -0500

    checkstyle

--- src/main/java/net/patchworkmc/patcher/Patchwork.java
+++ src/main/java/net/patchworkmc/patcher/Patchwork.java
@@ -14,10 +14,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.stream.Stream;
 
